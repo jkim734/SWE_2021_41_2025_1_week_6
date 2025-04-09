64fa51637ac64744f148af6b0e0a209589dcdb60
김재현 <71424602+jkim734@users.noreply.github.com>
2025-04-09 19:16:57 +0900

    Update arrange.sh

--- arrange.sh
+++ arrange.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-source_folder="files"
+sf="files"
 
-if [[ ! -d "$source_folder" ]]; then
-    echo "Error: The 'files' folder does not exist in the current directory."
+if [[ ! -d "$sf" ]]; then
     exit 1
 fi
 
-for file in "$source_folder"/*; do
+for file in "$sf"/*; do
     if [[ -f "$file" ]]; then
-        base_name=$(basename "$file")
+        bn=$(basename "$file")
         
-        first_char=$(echo "${base_name:0:1}" | tr '[:upper:]' '[:lower:]')
+        fc=$(echo "${bn:0:1}" | tr '[:upper:]' '[:lower:]')
         
-        if [[ "$first_char" =~ [a-z] ]]; then
-            mkdir -p "$first_char"
+        if [[ "$fc" =~ [a-z] ]]; then
+            mkdir -p "$fc"
             
-            mv "$file" "$first_char/"
+            mv "$file" "$fc/"
         fi
     fi
 done
-
-echo "Files have been successfully organized into folders based on their names."
